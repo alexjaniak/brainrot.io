ed3a5391d8ff982237264671f39b520efbcb57d8
alexjaniak <alexander.janiak.acc@outlook.com>
2023-04-01 18:57:17 -0400

    Added more panel styling

--- brainrot.io/src/App.tsx
+++ brainrot.io/src/App.tsx
@@ -4,19 +4,12 @@ import './App.css';
 function App() {
   const _ids = ["7214464475577978118", "7216831717565385986", "7187800160297078021", "7204542501951212805"];
 
-  const iframe = { 
-    width: "100%",
-    height: "500px",
-    border: 0,
-  };
-
   return (
     <div className="App">
-      <h1>Brainrot.io</h1>
       <div id="panel">
         {_ids.map((_id) => {
           const embed_src = "https://www.tiktok.com/embed/" + _id;
-          return <iframe src={embed_src} style={iframe}></iframe>;
+          return <iframe className="stream" src={embed_src} scrolling="no"></iframe>;
         })}
       </div>
     </div>
