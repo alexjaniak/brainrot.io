36e66e6ccf4fd2e633f0b4e5942195ea0d3494ea
alexjaniak <alexander.janiak.acc@outlook.com>
2023-04-02 09:47:43 -0400

    pausing bug

--- brainrot.io/src/App.tsx
+++ brainrot.io/src/App.tsx
@@ -2,6 +2,10 @@ import { useState, useEffect} from 'react';
 import './App.css';
 import _ids from "./ids.json"
 
+var script = document.createElement('script');
+script.src = "https://code.jquery.com/jquery-3.6.3.min.js";
+
+
 class Queue<T> {
   public constructor(
       private elements: Record<number, T> = {},
@@ -39,24 +43,41 @@ function pullNextId(queue: Queue<string>) {
   return queue.dequeue();
 }
 
+function waitForRender() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 1000)
+  })
+}
+
+
 function App() {
   const queue = new Queue<string>()
   _ids.forEach((_id) => {
     queue.enqueue(_id)
-  })
-  
+  });
+
+  const changeVideo = async (event) => {
+    event.target.firstChild.src = "https://www.tiktok.com/embed/" + pullNextId(queue);
+    const a = await waitForRender();
+    console.log(event.target.firstChild.contents().find("css-uq19z6 e1hu91t76").text());
+    console.log(event.target.firstChild.contentWindow);
+    console.log(event.target.firstChild.contentDocument);
+    console.log(document.querySelector(".css-uq19z6"));
+  };
+
   const init = () => {
     const streams = [];
     for (let i = 0; i < 4; i++) {
       const embed_src = "https://www.tiktok.com/embed/" + pullNextId(queue);
-      streams.push(<div className="stream-container">
+      streams.push(<div className="stream-container" onClick={changeVideo}>
                       <iframe className="stream" src={embed_src} scrolling="no"></iframe>
                    </div>);
     }
     return streams
-  }
-
-  console.log(_ids);
+  };
+    
   return (
     <div className="App">
       <h1>Brain Rot</h1>
