f530d05e8379f535390fecd3dc2857fc773050e0
alexjaniak <alexander.janiak.acc@outlook.com>
2023-04-01 20:57:52 -0400

    added pulled ids

--- brainrot.io/src/App.tsx
+++ brainrot.io/src/App.tsx
@@ -1,16 +1,67 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
+import _ids from "./ids.json"
+
+class Queue<T> {
+  public constructor(
+      private elements: Record<number, T> = {},
+      private head: number = 0,
+      private tail: number = 0
+  ) { }
+
+  public enqueue(element: T): void {
+      this.elements[this.tail] = element;
+      this.tail++;
+  }
+
+  public dequeue(): T {
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+
+      return item;
+  }
+
+  public peek(): T {
+      return this.elements[this.head];
+  }
+
+  public get length(): number {
+      return this.tail - this.head;
+  }
+
+  public get isEmpty(): boolean {
+      return this.length === 0;
+  }
+}
+
+function pullNextId(queue: Queue<string>) {
+  return queue.dequeue();
+}
 
 function App() {
-  const _ids = ["7214464475577978118", "7216831717565385986", "7187800160297078021", "7204542501951212805"];
+  const queue = new Queue<string>()
+  _ids.forEach((_id) => {
+    queue.enqueue(_id)
+  })
+  
+  const init = () => {
+    const streams = [];
+    for (let i = 0; i < 4; i++) {
+      const embed_src = "https://www.tiktok.com/embed/" + pullNextId(queue);
+      streams.push(<div className="stream-container">
+                      <iframe className="stream" src={embed_src} scrolling="no"></iframe>
+                   </div>);
+    }
+    return streams
+  }
 
+  console.log(_ids);
   return (
     <div className="App">
+      <h1>Brain Rot</h1>
       <div id="panel">
-        {_ids.map((_id) => {
-          const embed_src = "https://www.tiktok.com/embed/" + _id;
-          return <iframe className="stream" src={embed_src} scrolling="no"></iframe>;
-        })}
+        {init()}
       </div>
     </div>
   )
---
+++ brainrot.io/src/scrape.jsx
@@ -0,0 +1,58 @@
+import webdriver from selenium;
+import By from selenium.webdriver.common.by; 
+import WebDriverWait from selenium.webdriver.support.ui; 
+import expected_conditions from selenium.webdriver.support; 
+import webdriver from selenium;
+import Service from selenium.webdriver.chrome.service; 
+import ChromeDriverManager from webdriver_manager.chrome; 
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+class Queue {
+    constructor() {
+      this.elements = {};
+      this.head = 0;
+      this.tail = 0;
+    }
+    enqueue(element) {
+      this.elements[this.tail] = element;
+      this.tail++;
+    }
+    dequeue() {
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+      return item;
+    }
+    peek() {
+      return this.elements[this.head];
+    }
+    get length() {
+      return this.tail - this.head;
+    }
+    get isEmpty() {
+      return this.length === 0;
+    }
+  }
+
+//set chromodriver.exe path
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()));
+driver.get('https://www.tiktok.com/foryou/?lang=en');
+WebDriverWait(driver, 20).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "tiktok-unxpj7-DivCloseWrapper"))).click();
+print("Page title is: ");
+sleep(1000);
+WebDriverWait(driver, 20).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "tiktok-kd7foj-DivVideoWrapper"))).click();
+sleep(1000);
+names = new Queue();
+let name1 = driver.find_element(By.CLASS_NAME, "tiktok-1v8b11s-PCopyLinkText").text;
+name1 = name1.split("/")[5].split("?")[0];
+names.enqueue(name1);
+for (let i = 0; i < 10; i++) {
+    WebDriverWait(driver, 20).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "tiktok-1sltbs0-ButtonBasicButtonContainer-StyledVideoSwitch"))).click()
+    time.sleep(.5)
+    name1 = driver.find_element(By.CLASS_NAME, "tiktok-1v8b11s-PCopyLinkText").text
+    name1 = name1.split("/")[5].split("?")[0]
+    names.enqueue(name1)
+}
+console.log(names)
